5faa9b697293cb8c59b1eae407da33f0ee2764c7
bsnun8 <bishnunath018@gmail.com>
2022-06-01 08:26:17 +0600

    Simple Mathematical operations in JavaScript

---
+++ mathematical-operation.js
@@ -0,0 +1,18 @@
+var onionPrice = 43;
+var eggPrice = 10;
+var eggQuantity = 7;
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var eggPriceMultiplication = eggPrice * eggQuantity;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+// console.log(totalPrice);
+// console.log(priceDifference);
+// console.log(eggPriceMultiplication);
+
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice;
+// console.log(orangeQuantity);
